238246d87a5e507343c8bd519d02b287ea147f9e
Colm <17203376@studentmail.ul.ie>
2019-10-25 09:33:17 +0100

    Added an activity page for stat roll selection with a basic layout using placeholder images, needs actual code added along with a checkbox or slider for extra roll management

---
+++ app/src/main/java/com/example/dnd5ecompanion/StatRollerStyleSelection5e.java
@@ -0,0 +1,14 @@
+package com.example.dnd5ecompanion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatRollerStyleSelection5e extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stat_roller_style_selection5e);
+    }
+}
