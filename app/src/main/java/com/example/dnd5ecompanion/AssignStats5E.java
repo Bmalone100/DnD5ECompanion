cc3b4ea2b73209ffaa6f14a10c9684e86c93186e
Colm <17203376@studentmail.ul.ie>
2019-11-01 10:14:40 +0000

    Added Stat writing boxes and the initialisation of the bundle to the the stat assign page.

--- app/src/main/java/com/example/dnd5ecompanion/AssignStats5E.java
+++ app/src/main/java/com/example/dnd5ecompanion/AssignStats5E.java
@@ -2,7 +2,9 @@ package com.example.dnd5ecompanion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AssignStats5E extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class AssignStats5E extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assign_stats5e);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(StatRollerStyleSelection5e.EXTRA_MESSAGE);
+
+        Bundle stats = new Bundle();
+
+        TextView StatAssignStatList = findViewById(R.id.StatAssignStatList);
+        StatAssignStatList.setText(message);
     }
 }
