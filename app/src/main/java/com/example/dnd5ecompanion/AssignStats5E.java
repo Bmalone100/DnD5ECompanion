306afd24f1f84ed2caee2c3fe473609256e02d71
Colm <17203376@studentmail.ul.ie>
2019-11-01 09:40:43 +0000

    Added an activity page for stat roll selection with a basic layout using placeholder images, needs actual code added along with a checkbox or slider for extra roll management

---
+++ app/src/main/java/com/example/dnd5ecompanion/AssignStats5E.java
@@ -0,0 +1,14 @@
+package com.example.dnd5ecompanion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AssignStats5E extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assign_stats5e);
+    }
+}
--- app/src/main/java/com/example/dnd5ecompanion/StatRollerStyleSelection5e.java
+++ app/src/main/java/com/example/dnd5ecompanion/StatRollerStyleSelection5e.java
@@ -2,13 +2,120 @@ package com.example.dnd5ecompanion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.CheckBox;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class StatRollerStyleSelection5e extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myapplication.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stat_roller_style_selection5e);
     }
+
+    int rollAmount = 0;
+    int rolled = 0;
+    int lowRoll = 0;
+    int randomRoll = 0;
+
+    public void extraRollChecked(View view)
+    {
+        boolean checked = ((CheckBox) view).isChecked();
+
+        switch(view.getId()) //Reason for switch is too allow more checkboxes in future
+        {
+            case R.id.statSelect5EExtraRollCheck:
+                if (checked)
+                {
+                    rollAmount = 7;
+                }
+                else rollAmount = 6;
+                break;
+        }
+    }
+
+    public void roll1d20(View view) {
+        String message = "";
+        for(int i =0; i < rollAmount; i++) {
+            rolled = (int) ((20 * Math.random()) + 1);
+            message += rolled + "  ";
+            rolled = 0;
+        }
+        Intent intent = new Intent(this, AssignStats5E.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void roll3d6(View view) {
+        String message = "";
+        for(int i =0; i < rollAmount; i++) {
+            rolled = (int) ((6 * Math.random()) + 1);
+            rolled += (int) ((6 * Math.random()) + 1);
+            rolled += (int) ((6 * Math.random()) + 1);
+            message += rolled + "  ";
+            rolled = 0;
+        }
+        Intent intent = new Intent(this, AssignStats5E.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void roll4d6(View view) {
+        String message = "";
+        for(int i =0; i < rollAmount; i++) {
+            rolled = (int) ((6 * Math.random()) + 1);
+            rolled += (int) ((6 * Math.random()) + 1);
+            rolled += (int) ((6 * Math.random()) + 1);
+            rolled += (int) ((6 * Math.random()) + 1);
+            message += rolled + "  ";
+            rolled = 0;
+        }
+        Intent intent = new Intent(this, AssignStats5E.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void roll4d6Low(View view) {
+        String message = "";
+        for(int i =0; i < rollAmount; i++) {
+            rolled = (int) ((6 * Math.random()) + 1);
+            lowRoll = rolled;
+            randomRoll = (int) ((6 * Math.random()) + 1);
+            if (randomRoll<lowRoll)
+            {
+                lowRoll = randomRoll;
+            }
+            rolled += randomRoll;
+            randomRoll = (int) ((6 * Math.random()) + 1);
+            if (randomRoll<lowRoll)
+            {
+                lowRoll = randomRoll;
+            }
+            rolled += randomRoll;
+            randomRoll = (int) ((6 * Math.random()) + 1);
+            if (randomRoll<lowRoll)
+            {
+                lowRoll = randomRoll;
+            }
+            rolled += randomRoll;
+            rolled = rolled - lowRoll;
+            message += rolled + "  ";
+            rolled = 0;
+        }
+        Intent intent = new Intent(this, AssignStats5E.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void statHelp(View view) {
+            Snackbar.make(view, "This page lets you select what set of dice are rolled to give you your stat numbers. /n The checkbox lets you add a 7th spare dice roll to the normal 6.", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Action", null).show();
+        }
+
 }
+
