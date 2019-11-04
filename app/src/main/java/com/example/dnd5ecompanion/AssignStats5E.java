01e43e1a1299d16139c89a7def00705cd6e2ed4e
Bmalone100 <36305546+Bmalone100@users.noreply.github.com>
2019-11-04 16:41:40 +0000

    Revert "Revert "Authentication""
    This reverts commit d48b7ea3bdcd5a903f7063b9540a479a4d093c62.

---
+++ app/src/main/java/com/example/dnd5ecompanion/AssignStats5E.java
@@ -0,0 +1,32 @@
+package com.example.dnd5ecompanion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class AssignStats5E extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assign_stats5e);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(StatRollerStyleSelection5e.EXTRA_MESSAGE);
+
+        Bundle stats = new Bundle();
+
+        TextView StatAssignStatList = findViewById(R.id.StatAssignStatList);
+        StatAssignStatList.setText(message);
+    }
+
+    public void statHelp(View view) {
+        Snackbar.make(view, "This page lets you assign your stats, the stats you rolled on the previous page are shown on the right. /nThe racial bonuses are also displayed between the two.", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Action", null).show();
+    }
+}
