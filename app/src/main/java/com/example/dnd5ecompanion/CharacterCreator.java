495a5126c073592f641ff8739c1423e2ff94c3ae
Bmalone100 <36305546+Bmalone100@users.noreply.github.com>
2019-11-28 11:57:58 +0000

    RecyclerBeginning
    Attempt at list

--- app/src/main/java/com/example/dnd5ecompanion/CharacterCreator.java
+++ app/src/main/java/com/example/dnd5ecompanion/CharacterCreator.java
@@ -13,11 +13,16 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.example.dnd5ecompanion.CharacterViewerAdapter.listOfCharacters;
 
 public class CharacterCreator extends AppCompatActivity {
     //Variables Needed for class methods
@@ -69,8 +74,26 @@ public class CharacterCreator extends AppCompatActivity {
         characterSheet aCharacter = new characterSheet(id,name,race);
         myRef.push().setValue(aCharacter);
         //showPopupMessage(name + "Added");
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                characterSheet value = dataSnapshot.getValue(characterSheet.class);
+                System.out.println(value);
+                listOfCharacters.add(value);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
         //Return to main
-        Intent mainIntent = new Intent(this, MainActivity.class);
-        startActivity(mainIntent);
+        //Intent mainIntent = new Intent(this, MainActivity.class);
+        //startActivity(mainIntent);
     }
 }
